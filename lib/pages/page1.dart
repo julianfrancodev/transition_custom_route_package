bf07088fbab45841a849b7db2e3588a2c82e2c69
julian <juliandanielfranco@gmail.com>
2021-01-25 10:17:29 -0500

    feat: improve animations

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:transition_custom_route/helpers/route_transitions.dart';
+import 'package:transition_custom_route/pages/page2.dart';
 
 class Page1 extends StatelessWidget {
   @override
@@ -10,12 +12,23 @@ class Page1 extends StatelessWidget {
       ),
       backgroundColor: Colors.blueGrey,
       body: Center(
-        child: MaterialButton(
+          child: MaterialButton(
+        child: Text("Go to page 2"),
+        color: Colors.white,
+        onPressed: () {
+          // Navigator.push(context, MaterialPageRoute(builder: (_) => Page2()));
+          
+          // Navigator.pushNamed(context, '/page2');
+          //
+          RouteTransition(
+            context: context,
+            child: Page2(),
+            animation: AnimationType.fadeIn,
+            duration: Duration(milliseconds: 300),
+          );
 
-
-          onPressed: (){},
-        )
-      ),
+        },
+      )),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:transition_custom_route/main.dart';
+import 'package:transition_custom_route/pages/page1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Page1());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
